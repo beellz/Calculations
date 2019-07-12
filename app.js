41d5588ff2289aa9839f7d99909fd17a3ac78a9d
beellz <belal20795@gmail.com>
2019-07-12 22:04:12 +0530

    trying the addition

--- app.js
+++ app.js
@@ -1,16 +1,89 @@
 
 
-function equals() {
-    //to do is taking the number in both the fields 
+//to do is taking the number in both the fields 
 
 
 
-    // make it connect with the option 
+const num1 = document.getElementById('First1');
+const num2 = document.getElementById("Second2");
+
+num1.addEventListener('input', function() {
+    console.log(num1.value);
+    const numTemp = num1.value;
+
+});
+
+num2.addEventListener('input', function() {
+    console.log(num2.value);
+    const numTemp = num2.value;
+
+});
 
 
-    // evaluate the answer
+
+// make it connect with the option 
+
+// function getSelectValue () {
+//     var seletedValue = document.getElementById("list").value;
+//     console.log(seletedValue);
+// };
+
+
+// evaluate the answer
+
+// if (selectedValue = addition ) {
+//     var x = num1 + num2;
+//     document.getElementById("comp").innerHTML = x ;
+// }
+// else {
+//     var y = num1 - num2;
+
+// };
+
+
+// return the answer at the bottom
+
+// function ValueAnswer () {
+//      var x = document.getElementById("comp").innerHTML;
+//      var q = x + so;
+//     console.log(q);
+// };
+
+// function ValueAnswer () {
+//     console.log(Num1);
+// };
+
+// function valveas () {
+//     console.log("hello to u ")
+    
+//     console.log(count);
+// };
+// var count = [];
+
+// num1.addEventListener('input', valveas);
 
 
 
-    // return the answer at the bottom
-}
+// function show_selected() {
+//     var selector = document.getElementById('list');
+//     var value = selector[selector.selectedIndex].value;
+
+//     document.getElementById('answer').innerHTML = value;
+// }
+
+// document.getElementById('equals').addEventListener('click', show_selected);
+
+function showAns () { 
+        var x = document.querySelector('select').value ;
+        
+        if (x = addition) {
+            console.log("hello")
+            document.getElementById('answer').innerHTML = value;
+        }
+        else {
+            console.log("no hello")
+        }
+        
+    };
+
+    document.getElementById('equals').addEventListener('click', showAns);
