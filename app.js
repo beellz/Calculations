3cdcdd52667b1fc8334b5c1bf6c397bdaabb3a5e
beellz <belal20795@gmail.com>
2019-07-12 19:00:39 +0530

    added a box for answer

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+function equals() {
+    //to do is taking the number in both the fields 
+
+
+
+    // make it connect with the option 
+
+
+    // evaluate the answer
+
+
+
+    // return the answer at the bottom
+}
